061948cdac774442ea84d7d33a68cb2917179c75
awakelife93 <awakelife93@gmail.com>
2025-05-26 17:09:37 +0900

    feature: add user signup welcome event

---
+++ src/main/kotlin/com/example/demo/user/event/UserEventHandler.kt
@@ -0,0 +1,43 @@
+package com.example.demo.user.event
+
+import com.example.demo.infrastructure.kafka.provider.KafkaTopicMetaProvider
+import com.example.demo.infrastructure.mail.MailPayload
+import com.example.demo.infrastructure.webhook.WebHookProvider
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
+
+private val logger = KotlinLogging.logger {}
+
+@Component
+class UserEventHandler(
+	private val mailKafkaTemplate: KafkaTemplate<String, MailPayload>,
+	private val webHookProvider: WebHookProvider
+) {
+	@Async
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	fun handleWelcomeSignUpEvent(welcomeSignUpEvent: WelcomeSignUpEvent) {
+		runCatching {
+			val payload =
+				MailPayload.of(
+					to = welcomeSignUpEvent.email,
+					subject = "${welcomeSignUpEvent.name}.",
+					body = "Welcome to our service!"
+				)
+
+			mailKafkaTemplate.send(KafkaTopicMetaProvider.MAIL_TOPIC, payload)
+		}.onFailure { exception ->
+			logger.error(exception) { "Failed to send message to Kafka" }
+
+			webHookProvider.sendSlack(
+				title = "Failed to send message to Kafka (handleWelcomeSignUpEvent)",
+				lines = mutableListOf("Failed to send message to Kafka: ${exception.message} / $welcomeSignUpEvent")
+			)
+
+			throw exception
+		}
+	}
+}
---
+++ src/main/kotlin/com/example/demo/user/event/WelcomeSignUpEvent.kt
@@ -0,0 +1,18 @@
+package com.example.demo.user.event
+
+import com.example.demo.user.entity.User
+
+data class WelcomeSignUpEvent(
+	val email: String,
+	val name: String
+) {
+	companion object {
+		fun from(user: User): WelcomeSignUpEvent =
+			with(user) {
+				WelcomeSignUpEvent(
+					email = email,
+					name = name
+				)
+			}
+	}
+}
